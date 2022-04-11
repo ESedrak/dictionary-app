155c462b7ecd7a0d9c548ca6b2f3939a78b2fd6b
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-11 16:15:42 +1000

    Added axios + integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
 
+  const handleKeywordSearch = (response) => {
+    alert(response.data[0].word);
+  };
+
   const handleSearch = (e) => {
     e.preventDefault();
-    alert(`Searching for the word ${keyword}`);
+
+    let apiKey = ` https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiKey).then(handleKeywordSearch);
   };
 
   const keywordChange = (e) => {
