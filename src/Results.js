da64d9d70f72a084feb97f1316fd50725b6f9f25
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-13 11:12:19 +1000

    Setup Phonetics with sound(bug found - adds an extra 'Listen button' that opens it up to the dictionary app page.

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
-  // console.log(props.results);
+  // console.log(props.results.phonetics);
   // console.log(props.results.meanings[0].definitions[0].definition);
   if (props.results) {
     return (
       <div>
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map((phonetic, index) => {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map((meaning, index) => {
           return (
             <div key={index}>
