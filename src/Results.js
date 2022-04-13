8e492d4e8bbf99009a0b3fa434d646fed3d81926
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-13 11:31:02 +1000

    Added .filter to fix empty audio bug

--- src/Results.js
+++ src/Results.js
@@ -9,13 +9,17 @@ export default function Results(props) {
     return (
       <div>
         <h2>{props.results.word}</h2>
-        {props.results.phonetics.map((phonetic, index) => {
-          return (
-            <div key={index}>
-              <Phonetics phonetic={phonetic} />
-            </div>
-          );
-        })}
+        {props.results.phonetics
+          .filter((phonetic) => {
+            return phonetic.audio;
+          })
+          .map((phonetic, index) => {
+            return (
+              <div key={index}>
+                <Phonetics phonetic={phonetic} />
+              </div>
+            );
+          })}
         {props.results.meanings.map((meaning, index) => {
           return (
             <div key={index}>
